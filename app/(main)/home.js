9fe7238211d083d04873250c15e0fb25c361e133
umergillani36 <syedomershahgillani@gmail.com>
2024-07-05 16:11:38 +0500

    home screen ui done

---
+++ app/(main)/home.js
@@ -0,0 +1,138 @@
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { colors } from "../../resources/colors";
+import { Searchbar, Text } from "react-native-paper";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import ScheduleCard from "../../components/ScheduleCard/ScheduleCard";
+import Chip from "../../components/Chip/Chip";
+import Table from "../../components/Table/Table";
+import LocationCard from "../../components/LocationCard/LocationCard";
+
+const home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.headerContent}>
+        <Text variant="headlineMedium" style={styles.heading}>
+          Today's Schedule
+        </Text>
+        <Text variant="headlineMedium" style={styles.heading}>
+          3 June
+        </Text>
+        <View style={styles.filterContainer}>
+          <Searchbar
+            placeholder="Search"
+            mode="bar"
+            style={styles.searchInput}
+            onChangeText={setSearchQuery}
+            selectionColor={colors.tealA100}
+            value={searchQuery}
+          />
+          <TouchableOpacity style={styles.filterIconContainer}>
+            <AntDesign name="filter" size={26} color={colors.teal900} />
+          </TouchableOpacity>
+        </View>
+        <Text variant="headlineLarge" style={styles.resultHeading}>
+          Results
+        </Text>
+        <ScheduleCard />
+        <ScheduleCard />
+        <ScheduleCard />
+        <Text variant="headlineLarge" style={styles.resultHeading}>
+          Total Results
+        </Text>
+        <Chip title="Total Patients" value="28/35" bgColor="white" />
+        <Chip title="Total Locations" value="2" bgColor="white" />
+        <Chip title="3 weeks since last visit" value="3" bgColor="white" />
+        <Chip title="With Results" value="5" bgColor="white" />
+        <Chip title="Requested Follow ups" value="2" bgColor="white" />
+        <View style={styles.locationHeading}>
+          <Text variant="headlineLarge" style={{ ...styles.heading }}>
+            Location #1
+          </Text>
+          <Text variant="bodyLarge" style={styles.locationTiming}>
+            {"(9:00 - 12:00)"}
+          </Text>
+        </View>
+        <Text variant="headlineSmall" style={styles.resultHeading}>
+          Nursing Home Address
+        </Text>
+        <LocationCard />
+        <LocationCard />
+        <LocationCard />
+        <View style={styles.locationHeading}>
+          <Text variant="headlineLarge" style={{ ...styles.heading }}>
+            Location #2
+          </Text>
+          <Text variant="bodyLarge" style={styles.locationTiming}>
+            {"(12:30 - 5:00)"}
+          </Text>
+        </View>
+        <Text variant="headlineSmall" style={styles.resultHeading}>
+          Nursing Home Address
+        </Text>
+        <LocationCard />
+        <LocationCard />
+        <LocationCard />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.teal300,
+    padding: 20,
+  },
+  headerContent: {
+    width: "100%",
+    alignItems: "center",
+    gap: 10,
+    paddingBottom: 40,
+  },
+  heading: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  filterContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  searchInput: {
+    width: "80%",
+    borderRadius: 12,
+    backgroundColor: "white",
+  },
+  filterIconContainer: {
+    width: 55,
+    height: 55,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  resultHeading: {
+    width: "100%",
+    textAlign: "left",
+    marginVertical: 10,
+    fontWeight: "bold",
+    color: "white",
+  },
+  locationHeading: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    marginTop: 20,
+    gap: 4,
+  },
+  locationTiming: {
+    color: "white",
+    marginBottom: 5,
+    fontWeight: "bold",
+  },
+});
