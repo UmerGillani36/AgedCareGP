869141614babcc1905c67ab08e2bd423b52ceb3a
umergillani36 <syedomershahgillani@gmail.com>
2024-07-08 14:16:55 +0500

    constants

---
+++ resources/constant.js
@@ -0,0 +1,32 @@
+export const loremText = `
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit. Nunc at
+feugiat nisi. Etiam placerat varius
+vehicula. Nam vitae hendrerit lorem, eu
+sagittis elit. Sed quis odio vitae turpis
+porta posuere facilisis nec lectus. Sed
+facilisis feugiat enim ut posuere.
+Vestibulum vitae nibh a eros
+scelerisque fringilla a nec elit. Etiam ac
+nisl egestas, facilisis ipsum sit amet,
+dignissim purus. Proin vitae ultricies
+massa. Phasellus semper tellus et felis
+dignissim, non aliquet metus sodales.
+Fusce a leo vel leo congue pulvinar.
+Mauris nec sem mi. Nam quis quam ut
+dui consectetur luctus. Cras eleifend,
+nibh in tincidunt pretium, nibh arcu
+feugiat justo, at condimentum eros
+mauris sed nunc. Nam ac finibus
+metus.
+
+Nam blandit libero convallis ipsum
+aliquet efficitur. Duis a ullamcorper
+lectus. Vivamus quis urna posuere,
+gravida tortor ac, finibus risus. Proin
+dignissim interdum libero, in ultrices
+leo. Aenean ac odio arcu. Nunc
+convallis nisl id ex auctor efficitur. Sed
+id lacus nec nunc varius ultricies id
+commodo urna.
+`;
